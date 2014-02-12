6a0f11bcb04af7690d112414a2baddc00ab084e8
William B. Wettersten <wbw20@case.edu>
2014-02-12 14:17:28 -0500

    remove some noise

--- Tabla/RadialView.m
+++ Tabla/RadialView.m
@@ -24,33 +24,6 @@
     CGContextMoveToPoint(context, 10, 500);
     CGContextAddArc(context, center.x, center.y, RADIUS, M_PI * 2, M_PI, 0);
     CGContextStrokePath(context);
-
-    
-    // Seed the random number generator
-    srandom((int)time(NULL));
-    
-    // Create a path object
-    path = [[NSBezierPath alloc] init];
-    [path setLineWidth:3.0];
-    NSPoint p = NSMakePoint(0.0, 0.0);
-    [path moveToPoint:p];
-    int i;
-    for (i = 0; i < 500; i+= 100) {
-        p = NSMakePoint(i, i + 43);
-        [path lineToPoint:p];
-    }
-    [path closePath];
-    
-    NSBezierPath* aPath = [NSBezierPath bezierPath];
-    [aPath setLineWidth:3.0];
-    
-    [aPath moveToPoint:NSMakePoint(0.0, 0.0)];
-    [aPath lineToPoint:NSMakePoint(100.0, 100.0)];
-    [aPath curveToPoint:NSMakePoint(108.0, 210.0)
-          controlPoint1:NSMakePoint(60.0, 20.0)
-          controlPoint2:NSMakePoint(280.0, 100.0)];
-    [aPath stroke];
-
     
     return self;
 }
@@ -75,10 +48,6 @@
     // Fill the view with green
     [[NSColor blueColor] set];
     [NSBezierPath fillRect: bounds];
-    
-    // Draw the path in white
-    [[NSColor whiteColor] set];
-    [path stroke];
 }
 
 @end
