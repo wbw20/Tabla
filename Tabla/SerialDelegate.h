e3aaad1d39351676257f6faf56a3e467a74d20f7
William B. Wettersten <wbw20@case.edu>
2014-03-02 21:29:14 -0500

    jk

---
+++ Tabla/SerialDelegate.h
@@ -0,0 +1,13 @@
+//
+//  SerialDelegate.h
+//  Tabla
+//
+//  Created by William Wettersten on 3/2/14.
+//  Copyright (c) 2014 William Wettersten. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SerialDelegate : NSObjet
+
+@end
---
+++ Tabla/SerialDelegate.m
@@ -0,0 +1,13 @@
+//
+//  SerialDelegate.m
+//  Tabla
+//
+//  Created by William Wettersten on 3/2/14.
+//  Copyright (c) 2014 William Wettersten. All rights reserved.
+//
+
+#import "SerialDelegate.h"
+
+@implementation SerialDelegate
+
+@end
