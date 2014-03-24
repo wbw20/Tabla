fe56a7f2bd253c298f35aa30b7d22d846d8a62fc
William B. Wettersten <wbw20@case.edu>
2014-03-24 16:17:05 -0400

    new layout

---
+++ Tabla/Window.h
@@ -0,0 +1,13 @@
+//
+//  Window.h
+//  Tabla
+//
+//  Created by William Wettersten on 3/24/14.
+//  Copyright (c) 2014 William Wettersten. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Window : NSWindow
+
+@end
---
+++ Tabla/Window.m
@@ -0,0 +1,17 @@
+//
+//  Window.m
+//  Tabla
+//
+//  Created by William Wettersten on 3/24/14.
+//  Copyright (c) 2014 William Wettersten. All rights reserved.
+//
+
+#import "Window.h"
+
+@implementation Window
+
+-(IBAction) btnScanClicked:(id)sender {
+    NSLog(@"Testing");
+}
+
+@end
