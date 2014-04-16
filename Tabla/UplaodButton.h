35159cde80414392d19ffa279158e68b95ee3b6e
William B. Wettersten <wbw20@case.edu>
2014-04-16 12:33:35 -0400

    add in toolbar library

---
+++ Tabla/UplaodButton.h
@@ -0,0 +1,13 @@
+//
+//  UplaodButton.h
+//  Tabla
+//
+//  Created by William Wettersten on 4/16/14.
+//  Copyright (c) 2014 William Wettersten. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface UplaodButton : NSToolbarItem
+
+@end
---
+++ Tabla/UplaodButton.m
@@ -0,0 +1,17 @@
+//
+//  UplaodButton.m
+//  Tabla
+//
+//  Created by William Wettersten on 4/16/14.
+//  Copyright (c) 2014 William Wettersten. All rights reserved.
+//
+
+#import "UplaodButton.h"
+
+@implementation UplaodButton
+
+- (void) validate {
+    NSLog(@"something");
+}
+
+@end
